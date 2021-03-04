b700f32011acc0086a643e559feb4b919551a1e0
TheophileBonifaceAchille <theophile.bonifce@gmail.com>
2021-03-04 18:16:14 +0100

    pour micka

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
         <>
         <header className="bg-danger d-flex flex-row text-white p-5 shadow">
-            <NavLink to="/" className=""> Home </NavLink>
             <h1 className="text-center">Perfect Maze</h1>
         </header>
         </>
--- src/components/Maze.js
+++ src/components/Maze.js
@@ -1,70 +1,49 @@
 import React, { useEffect, useState } from 'react'
 
-const Maze = () => {
+const Maze = (props) => {
 
-    const [array, setArray] = useState([])
-    
-    const NUMBER_ROW = 5
-    const NUMBER_COL = 5
+  const [array, setArray] = useState([])
+
+  const NUMBER_ROW = props.x
+  const NUMBER_COL = props.y
 
     useEffect(() => {
-
-        let board = []
-
-        /* const removeWall = (row, col) => {
-            const cell = '#r' + row + 'c' + col
-            
-            if (row === 1) {
-                 if (col === NUMBER_COL)
-                     return;
-                 cell.css("border-right-style", "hidden")
-             }
-             else if (col === NUMBER_COL) {
-                 cell.css("border-top-style", "hidden")
-             }
-             else {
-                 if (Math.random() >= 0.5) {
-                     cell.css("border-top-style", "hidden")
-                 }
-                 else {
-                     cell.css("border-right-style", "hidden")
-                 }
-             }
-         } */
-  
-            for (let i = 0; i < NUMBER_ROW; i++) {
-                let innerArray = []
-                for (let j = 0; j < NUMBER_COL; j++) {
-                    let obj = {}
-                    obj['x'] = i
-                    obj['y'] = j
-                    let temp = []
-                    innerArray.push(obj)
-                    temp.push(obj)
-                };
-                board.push(innerArray);
-            }
-            setArray(board)
-        }, [NUMBER_ROW, NUMBER_COL]
-    )
-  
-    console.log(array)
+      
+    let board = []
         
-        return (
-            <table className='position-relative m-5' >
-                <tbody className='position-absolute'>
-                    {array.map((item, index) => (
-                        <tr key={index}>
-                            {item.map((innerItem, innerIndex) => (
-                                <td key={innerIndex} className='border border-primary p-3'>
-                                    {innerItem.state}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        )
+    for (let i = 0; i < NUMBER_ROW; i++) {
+      let innerArray = []
+      for (let j = 0; j < NUMBER_COL; j++) {
+        let obj = {}
+        obj['x'] = i
+        obj['y'] = j
+        let temp = []
+        innerArray.push(obj)
+        temp.push(obj)
+      }
+      board.push(innerArray)
     }
+    setArray(board)
+    }, [NUMBER_ROW, NUMBER_COL])
 
-export default Maze;
+  return (
+    <>
+      <button className="btn btn-primary m-3" onClick={() => props.back(0)}>BACK</button>
+      <table className="position-relative m-5">
+        <tbody className="position-absolute">
+          {array.map((item, index) => (
+            <tr key={index}>
+              {item.map((innerItem, innerIndex) => (
+                <td key={innerIndex} className="border border-primary p-3">
+                  {innerItem.state}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Maze
